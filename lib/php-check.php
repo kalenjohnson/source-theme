e393eafa05e8a5ecccbef68e3692886cbe22dad8
Kalen Johnson <info@kalenjohnson.com>
2015-04-08 16:23:24 -0700

    Adding PHP check

---
+++ lib/php-check.php
@@ -0,0 +1,27 @@
+<?php
+$updatePhp = new WPUpdatePhp('5.4.0');
+
+if ( ! $updatePhp->does_it_meet_required_php_version(PHP_VERSION)) {
+  $stock_themes = array(
+    'twentyfifteen',
+    'twentyfourteen',
+    'twentythirteen',
+    'twentytwelve',
+    'twentyeleven',
+    'twentyten'
+  );
+  foreach ($stock_themes as $theme) {
+    if (file_exists(WP_CONTENT_DIR . '/' . $theme . '/style.css')) {
+      switch_theme($theme);
+      break;
+    }
+
+    /**
+     * Check the standard directory in case WP_CONTENT_DIR is changed
+     */
+    if (file_exists(ABSPATH . 'wp-content/themes/' . $theme . '/style.css')) {
+      switch_theme($theme);
+      break;
+    }
+  }
+}
