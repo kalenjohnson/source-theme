a807c6728fcd8332974b1adbefe5b45ab4fde1a4
Kalen Johnson <info@kalenjohnson.com>
2015-04-08 16:28:48 -0700

    Removing conditional tag check and any sidebar references. Not using a sidebar on this bad boy

--- lib/config.php
+++ lib/config.php
@@ -26,43 +26,6 @@ if (!defined('DIST_DIR')) {
 }
 
 /**
- * Define which pages shouldn't have the sidebar
- */
-function display_sidebar() {
-  static $display;
-
-  if (!isset($display)) {
-    $conditionalCheck = new ConditionalTagCheck(
-      /**
-       * Any of these conditional tags that return true won't show the sidebar.
-       * You can also specify your own custom function as long as it returns a boolean.
-       *
-       * To use a function that accepts arguments, use an array instead of just the function name as a string.
-       *
-       * Examples:
-       *
-       * 'is_single'
-       * 'is_archive'
-       * ['is_page', 'about-me']
-       * ['is_tax', ['flavor', 'mild']]
-       * ['is_page_template', 'about.php']
-       * ['is_post_type_archive', ['foo', 'bar', 'baz']]
-       *
-       */
-      [
-        'is_404',
-        'is_front_page',
-        ['is_page_template', 'template-custom.php']
-      ]
-    );
-
-    $display = apply_filters('source/display_sidebar', $conditionalCheck->result);
-  }
-
-  return $display;
-}
-
-/**
  * $content_width is a global variable used by WordPress for max image upload sizes
  * and media embeds (in pixels).
  *
