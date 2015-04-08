27e250e35131b802d847e78956c550964c6922e8
Kalen Johnson <info@kalenjohnson.com>
2015-04-08 14:21:23 -0700

    Setting up featured images

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,8 +1,18 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
     <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
+      <?php if (has_post_thumbnail()) : ?>
+        <div class="header-content">
+          <h1 class="entry-title"><?php the_title(); ?></h1>
+          <?php get_template_part('templates/entry-meta'); ?>
+        </div>
+        <div class="featured-image">
+          <?php the_post_thumbnail(); ?>
+        </div>
+      <?php else : ?>
+        <h1 class="entry-title"><?php the_title(); ?></h1>
+        <?php get_template_part('templates/entry-meta'); ?>
+      <?php endif; ?>
     </header>
     <div class="entry-content">
       <?php the_content(); ?>
