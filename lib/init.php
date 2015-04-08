67c156295483fd612a3a3c4f3cf4d4a8cfd3f51a
Kalen Johnson <info@kalenjohnson.com>
2015-04-08 16:28:12 -0700

    Removing post-format support

--- lib/init.php
+++ lib/init.php
@@ -29,10 +29,6 @@ function setup() {
   // http://codex.wordpress.org/Function_Reference/add_image_size
   add_theme_support('post-thumbnails');
 
-  // Add post formats
-  // http://codex.wordpress.org/Post_Formats
-  add_theme_support('post-formats', ['aside', 'gallery', 'link', 'image', 'quote', 'video', 'audio']);
-
   // Add HTML5 markup for captions
   // http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
   add_theme_support('html5', ['caption', 'comment-form', 'comment-list']);
