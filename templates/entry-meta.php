2f36ea90cd86081a80613592eedd48c0d617c899
kalenjohnson <info@kalenjohnson.com>
2015-04-27 01:34:13 -0700

    Added permalink to post dates

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,7 +1,9 @@
 <?php $tags = get_the_tag_list('', __(', ', 'source')); ?>
 <div class="meta-container">
   <div class="meta-box">
-    <time class="updated" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
+    <a href="<?php the_permalink(); ?>">
+      <time class="updated" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
+    </a>
     <p class="byline author vcard"><?php echo __('By', 'source'); ?> <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a></p>
   </div>
   <div class="meta-box">
