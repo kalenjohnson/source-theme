6c73a3187c61b8aec67441d928784f56c237bb9a
kalenjohnson <info@kalenjohnson.com>
2015-04-27 01:03:21 -0700

    Updating so search shows titles

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,2 +1,7 @@
-<?php the_content(); ?>
+<?php if(is_search()) : ?>
+  <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+  <?php the_excerpt(); ?>
+<?php else : ?>
+  <?php the_content(); ?>
+<?php endif; ?>
 <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'source'), 'after' => '</p></nav>']); ?>
